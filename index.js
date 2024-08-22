27205606b038e28fde3458e3909b0742a35d540a
saikat <saikatbanerjee124@gmail.com>
2024-08-22 21:20:31 +0530

    port changed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 // Define a port
-const port = 3000;
+const port = 80;
 
 // Define a route for the root URL ('/')
 app.get('/', (req, res) => {
